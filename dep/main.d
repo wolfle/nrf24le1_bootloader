74781d03cc2415432d3d8c65cf059e1b0f8bcd82
Bob Guo <wolfle@yahoo.com>
2016-01-26 00:42:00 +0800

    clear build files

--- dep/main.d
+++
@@ -1,5 +0,0 @@
-obj/main.rel: src/main.c src/main.h ../nRF24LE1_SDK/include/rf.h \
- ../nRF24LE1_SDK/include/reg24le1.h \
- ../nRF24LE1_SDK/_target_sdcc_nrf24le1_24/include/target_nrf24le1_sdk.h \
- ../nRF24LE1_SDK/include/memory.h include/protocol.h \
- ../nRF24LE1_SDK/include/interrupt.h ../nRF24LE1_SDK/include/delay.h
